307d0d73d5a1db666d14820539221d3f10954ec2
Jorge Luis Hita, Ph.D. <jluishita@gmail.com>
2025-05-18 19:21:00 +0200

    Added comments

--- matching/pairmatching.py
+++ matching/pairmatching.py
@@ -1,7 +1,13 @@
 import pandas as pd
-import numpy as np
 
 class NaivePairMatching():
+    """
+    Class that performs naive pair matching
+
+    Unless the number of covariates is small, this method is NOT expected to work well
+
+    It has been developed for illustrative purposes
+    """
 
     def __init__(self, df_treatment, df_control, y_treatment, y_control):
         self.df_treatment = df_treatment.copy()
@@ -14,6 +20,9 @@ class NaivePairMatching():
 
 
     def find_pair(self, element, element_index):
+        """
+        Finds an element in the control group with the same covariates
+        """
         pair = self.df_control.loc[self.df_control.eq(element).all(1)]
         if len(pair) == 0:
             self.discarded.append(element_index)
@@ -23,19 +32,23 @@ class NaivePairMatching():
 
     def add_outcomes_to_df(self, treatment_index, control_index):
         """
+        Adds outcomes of paired treatment and control elements
         """
         series = pd.DataFrame({
             'treatment' : [self.y_treatment.loc[treatment_index]],
             'control' : [self.y_control.loc[control_index]]
         })
-        
+
         self.df_paired_outcomes = pd.concat(
             [self.df_paired_outcomes,
-            series]
+            series], ignore_index=True
         )
 
 
     def add_pair_to_paired_control(self, pair):
+        """
+        Add paired outcomes to df_paired_outcomes dataframe
+        """
         self.df_paired_control = pd.concat(
             [self.df_paired_control,
             pair]
@@ -43,6 +56,9 @@ class NaivePairMatching():
 
 
     def pair_matching(self):
+        """
+        Main function. It is the only function that is supposed to be called by the user
+        """
         for treat in self.df_treatment.itertuples(index=False):
 
             treat_index = self.df_treatment.loc[self.df_treatment.eq(treat).all(1)].index[0]
@@ -52,8 +68,7 @@ class NaivePairMatching():
             pair_index = pair.index[0]
             self.df_control = self.df_control.drop([pair_index])
             self.add_outcomes_to_df(
-                treat_index, 
+                treat_index,
                 pair_index
             )
-            self.add_pair_to_paired_control(pair) 
-        
+            self.add_pair_to_paired_control(pair)
